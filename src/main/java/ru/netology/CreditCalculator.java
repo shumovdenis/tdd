da878c28e0ef730ff5cc52bab03c3a942b60c159
Denis Shumov <75227278+shumovdenis@users.noreply.github.com>
2021-06-01 02:27:28 +0300

    implementation methods

--- src/main/java/ru/netology/CreditCalculator.java
+++ src/main/java/ru/netology/CreditCalculator.java
@@ -6,14 +6,20 @@ public class CreditCalculator {
     }
 
     public double payment(double sumCredit, int months, double interest) {
-        return 0;
+        double monthInterest = interest * 0.01 / 12;
+        double payment = sumCredit * (monthInterest + (monthInterest / (Math.pow((1 + monthInterest), months) - 1)));
+        return payment;
     }
 
     public double totalPayment (double sumCredit, int month, double interest) {
-        return 0;
+        double monthInterest = interest * 0.01 / 12;
+        double totalPayment = 12 * sumCredit * (monthInterest + (monthInterest / (Math.pow((1 + monthInterest), month) - 1)));
+        return totalPayment;
     }
 
     public double overPayment(double sumCredit, int month, double interest) {
-        return 0;
+        double monthInterest = interest * 0.01 / 12;
+        double overPayment = 12 * sumCredit * (monthInterest + (monthInterest / (Math.pow((1 + monthInterest), month) - 1))) - sumCredit;
+        return overPayment;
     }
 }
